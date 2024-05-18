cf1ba3e5e149d95603f400a1c6db3d9283e5b245
shu <1930201775@qq.com>
2024-05-18 22:02:37 +0800

    fifth commit

--- src/main/java/com/lsm/git.java
+++ src/main/java/com/lsm/git.java
@@ -13,5 +13,6 @@ public class git {
     public static void main(String[] args) {
         System.out.println("123");
         System.out.println("1234");
+        System.out.println("123456");
     }
 }
