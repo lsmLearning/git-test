c7d8964892532381e83ca696c13e3eb85cbffb4a
shu <1930201775@qq.com>
2024-05-18 20:50:37 +0800

    初始代码

---
+++ src/main/java/com/lsm/git.java
@@ -0,0 +1,16 @@
+package com.lsm;
+
+/**
+ * ClassName: git
+ * Package: com.lsm
+ * Description:
+ *
+ * @Author lsm
+ * @Create 2024/5/18 20:43
+ * @Version 1.0
+ */
+public class git {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
